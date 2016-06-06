6df91a91af2a39c3a489d765d9ba0485a865d16b
Devadas Vijayan <Devadas.Vijayan@multichoice.co.za>
2016-06-06 13:47:04 +0530

    [Survey List] Display an action bar to display ‘Send SMS’ option.
    Needed to completely format the xml structure to the default generated xml structure for ‘basic activity’ by Android studio.

--- app/src/main/java/com/corpus/survey/SurveyListActivity.java
+++ app/src/main/java/com/corpus/survey/SurveyListActivity.java
@@ -3,7 +3,10 @@ package com.corpus.survey;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -35,6 +38,8 @@ public class SurveyListActivity extends AppCompatActivity {
                 fromColumns, toViews, 0);
 
         mSurveyList.setAdapter(mAdapter);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         if (null != getSupportActionBar())
         {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -50,6 +55,28 @@ public class SurveyListActivity extends AppCompatActivity {
                 startActivity(contentDetailsIntent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_survey_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_sendSMS) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
 
