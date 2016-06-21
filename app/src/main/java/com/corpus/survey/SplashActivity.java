6f642aed0fc9b65a6a54b55471a08cf0e9728330
Devadas Vijayan <Devadas.Vijayan@multichoice.co.za>
2016-06-21 17:28:04 +0530

    [Startup Splash] Loading the default settings values on startup. Also checking for network connection. If not connected, display error message and exit the app.

--- app/src/main/java/com/corpus/survey/SplashActivity.java
+++ app/src/main/java/com/corpus/survey/SplashActivity.java
@@ -1,25 +1,53 @@
 package com.corpus.survey;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
 
 /**
  * Created by devadas.vijayan on 6/20/16.
  */
-public class SplashActivity  extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         try {
+            PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+            ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+            if (networkInfo == null || !networkInfo.isConnected()) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setCancelable(false);
+                AlertDialog dialog = builder.create();
+                dialog.getListView();
+                builder.setTitle(getString(R.string.not_connected_warning_title));
+                builder.setMessage(getString(R.string.not_connected_warning_message));
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                builder.show();
+            } else {
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-        finish();
     }
 }
